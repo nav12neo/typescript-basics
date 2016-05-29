207cda6fb07e45cf417a20e026c7dcd5272fe639
naveen kumar <naveen.aitian@gmail.com>
2016-05-29 11:15:58 +0530

    added watchify, gulp-uglify and bebelify to setup the full typescript project.

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,19 @@
-var gulp = require("gulp");
-var browserify = require("browserify");
+var gulp = require('gulp');
+var browserify = require('browserify');
 var source = require('vinyl-source-stream');
-var tsify = require("tsify");
+var tsify = require('tsify');
+var sourcemaps = require('gulp-sourcemaps');
+var buffer = require('vinyl-buffer');
 var paths = {
     pages: ['src/*.html']
 };
 
-gulp.task("copy-html", function () {
+gulp.task('copyHtml', function () {
     return gulp.src(paths.pages)
-        .pipe(gulp.dest("dist"));
+        .pipe(gulp.dest('dist'));
 });
 
-gulp.task("build", ["copy-html"], function () {
+gulp.task('default', ['copyHtml'], function () {
     return browserify({
         basedir: '.',
         debug: true,
@@ -20,7 +22,11 @@ gulp.task("build", ["copy-html"], function () {
         packageCache: {}
     })
         .plugin(tsify)
+        .transform("babelify")
         .bundle()
         .pipe(source('bundle.js'))
-        .pipe(gulp.dest("dist"));
+        .pipe(buffer())
+        .pipe(sourcemaps.init({loadMaps: true}))
+        .pipe(sourcemaps.write('./'))
+        .pipe(gulp.dest('dist'));
 });
